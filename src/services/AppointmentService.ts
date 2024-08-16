2783484c998ef640e3a3c25228aa0955b18dd768
MAKSIM <max_air@bk.ru>
2024-08-16 15:41:24 +0300

    add requireFields function

--- src/services/AppointmentService.ts
+++ src/services/AppointmentService.ts
@@ -1,4 +1,7 @@
 import { useHttp } from "../hooks/http.hook"
+import hasRequiredFields from "../utils/hasRequiredFields";
+
+const requiredFields = ["id", "date", "name", "service", "phone", "canceled"];
 
 export const useAppointmentService = () => {
     const { loadingStatus, request } = useHttp();
@@ -6,7 +9,14 @@ export const useAppointmentService = () => {
 
 
     const getAllAppointments = async () => {
-        const res = await request({ url: _apiBase })
+        const res = await request({ url: _apiBase });
+        if (Array.isArray(res) && res.every(item => {
+            return hasRequiredFields(item, requiredFields)
+        })) {
+            return res;
+        } else {
+            throw new Error("Data doesn't have all fields");
+        }
     }
 
 
