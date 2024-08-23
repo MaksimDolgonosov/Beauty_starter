25657d21831e28e95123c660f0210d9b9c372802
MAKSIM <max_air@bk.ru>
2024-08-23 15:32:48 +0300

    homework-done

--- src/components/appointmentList/AppointmentList.tsx
+++ src/components/appointmentList/AppointmentList.tsx
@@ -3,7 +3,7 @@ import { useEffect, useContext } from "react";
 import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
 import { ActiveAppointment } from "../../shared/interfaces/appointment.interface";
 import Spinner from "../spinner/Spinner";
-
+import Error from "../error/Error";
 
 function AppointmentList() {
 	const { allActiveAppointments, getAllActiveAppointments, loadingStatus } = useContext(AppointmentContext);
@@ -11,9 +11,21 @@ function AppointmentList() {
 	useEffect(() => {
 		getAllActiveAppointments();
 	}, [])
+
+	const reload: React.MouseEventHandler = (e): void => {
+		e.preventDefault();
+		getAllActiveAppointments();
+	}
 	// console.log(allActiveAppointments)
 	return (
 		<>
+			{loadingStatus === "error" ?
+				<>
+					<Error />
+					<button className="schedule__reload" onClick={e => reload(e)}>Загрузить</button>
+				</>
+
+				: null}
 			{loadingStatus === "loading" ? <Spinner /> : null}
 			{allActiveAppointments.map(({ id, date, name, service, phone }: ActiveAppointment) => {
 				return <AppointmentItem id={id} key={id} date={date} name={name} service={service} phone={phone} />
