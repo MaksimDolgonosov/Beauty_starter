81d96f75395bad8b8fe52a7d6f852a3a24bb8cec
MAKSIM <max_air@bk.ru>
2024-09-12 16:51:55 +0300

    History doing 2

--- src/components/historyList/HistoryList.tsx
+++ src/components/historyList/HistoryList.tsx
@@ -1,14 +1,28 @@
 import AppointmentItem from "../appointmentItem.tsx/AppointmentItem";
+import { useContext, useEffect } from "react";
+import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
+import Spinner from "../spinner/Spinner";
+import { IAppointment } from "../../shared/interfaces/appointment.interface";
+
+interface IHistoryProps extends IAppointment {
+
+}
 
 function HistoryList() {
+	const { getAllAppointments, allAppointments, loadingStatus } = useContext(AppointmentContext);
+
+	useEffect(() => {
+		console.log("history")
+		getAllAppointments();
+		console.log(allAppointments)
+		// .then((res: IAppointment[]) => console.log(res))
+	}, [])
 	return (
 		<>
-			{/* <AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem />
-			<AppointmentItem /> */}
+			{loadingStatus ? <Spinner /> :
+				allAppointments.map(item => {
+					return <AppointmentItem id={item.id} date={item.date} name={item.name} phone={item.phone} canceled={item.canceled} openModal={() => { }} service={item.service} />
+				})}
 		</>
 	);
 }
